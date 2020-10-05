a7f2a2a6788468399d940651d80d692d56deab6a
rafsan <rafsanamin18@gmail.com>
2020-10-05 10:57:01 +0600

    send data to mongodb using fake data

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mypty.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+    const eventsCollection = client
+        .db(process.env.DB_NAME)
+        .collection("events");
+
+    app.post("/addEvents", (req, res) => {
+        const event = req.body;
+        console.log(event);
+        eventsCollection.insertMany(event).then((result) => {
+            res.send(result);
+        });
+    });
 });
 
 app.listen(port);
