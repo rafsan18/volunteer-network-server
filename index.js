41cba3395a558f72cb47fd2314305650fb18d74a
rafsan <rafsanamin18@gmail.com>
2020-10-07 02:44:39 +0600

    can delete object

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
+const ObjectId = require("mongodb").ObjectID;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mypty.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
@@ -18,22 +19,61 @@ const client = new MongoClient(uri, {
 });
 
 client.connect((err) => {
-    const eventsCollection = client
+    const eventsCollections = client
         .db(process.env.DB_NAME)
         .collection("events");
+    const chosenEventCollections = client
+        .db(process.env.DB_NAME)
+        .collection("chosenEventCollection");
 
+    // Post
     app.post("/addEvents", (req, res) => {
         const event = req.body;
         console.log(event);
-        eventsCollection.insertMany(event).then((result) => {
+        eventsCollections.insertMany(event).then((result) => {
             res.send(result);
         });
     });
 
-    app.get("/events", (req, res) => {
-        eventsCollection.find({}).toArray((err, documents) => {
+    app.post("/addChosenEvent", (req, res) => {
+        const chosenEvent = req.body;
+        chosenEventCollections.insertOne(chosenEvent).then((result) => {
+            res.send(result);
+        });
+    });
+
+    //Get
+    app.get("/chosenEvent", (req, res) => {
+        chosenEventCollections.find({}).toArray((err, documents) => {
             res.send(documents);
         });
+    });
+
+    app.get("/events", (req, res) => {
+        eventsCollections.find({}).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
+
+    app.get("/event/:id", (req, res) => {
+        eventsCollections
+            .find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0n]);
+            });
+    });
+
+    app.get("/", (req, res) => {
+        res.send("Welcome to Volunteer network");
+    });
+
+    // Delete
+    app.delete("/cancelEvent/:id", (req, res) => {
+        chosenEventCollections
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then((result) => {
+                console.log(result);
+            });
     });
 });
 
